ab59530b2b90bad6baf21b93522be8a3a072cba5
Daniel Klauser <daniel.klauser@hslu.ch>
2022-09-09 15:08:22 +0200

    bugfix

--- coralIcarusInterface.h
+++ coralIcarusInterface.h
@@ -3,12 +3,10 @@
 #ifndef CORAL_ICARUS_INFERENCER_H
 #define CORAL_ICARUS_INFERENCER_H
 
-
 #include "tensorResultToPassOnFloat.h"
 #include "tensorResultToPassOn.h"
 #include "tensorResultToPassOnUint8.h"
-#include "coralIcarusInference.h"
-
+#include "src/coralIcarusInference.h"
 
 class coralIcarusInference
 {
@@ -16,12 +14,12 @@ public:
     coralIcarusInference();
     ~coralIcarusInference();
 
-    void init(const std::string& modelPath,
-        std::vector<int32_t>& outImageDimensions,
-        int use_nnapi,
-        int num_threads);
+    void init(const std::string &modelPath,
+              std::vector<int32_t> &outImageDimensions,
+              int use_nnapi,
+              int num_threads);
 
-    void inference(const std::vector<uint8_t>& inputImage, std::vector<tensorResultToPassOn*>& outResults);
+    void inference(const std::vector<uint8_t> &inputImage, std::vector<tensorResultToPassOn *> &outResults);
 };
 
 #endif
